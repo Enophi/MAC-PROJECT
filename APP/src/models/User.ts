5e0b5ee826f2b9145a268c8dc84cbb8b229dbf7b
Alain Gobet <alain.gobet@heig-vd.ch>
2018-12-12 15:19:31 +0100

    Users

--- APP/src/models/User.ts
+++ APP/src/models/User.ts
@@ -1,8 +1,38 @@
 module.exports = {
-
+    id: {
+        type: 'uuid',
+        primary: true
+    },
     name: 'string',
     firstname:'string',
-    mail:'string'
-
+    email:'string',
     //TODO : Complete
+    published: {
+        type:"relationship",
+        target:"Recipe",
+        relationship:"PUBLISH",
+        direction:"out",
+        eager: true
+    },
+    likedRecipes: {
+        type:"relationship",
+        target:"Recipe",
+        relationship:"LIKE",
+        direction:"out",
+        eager: true
+    },
+    likedIngredients: {
+        type:"relationship",
+        target:"Ingredient",
+        relationship:"LIKE",
+        direction:"out",
+        eager: true
+    },
+    followed: {
+        type:"relationship",
+        target:"User",
+        relationship:"FOLLOW",
+        direction:"out",
+        eager: true
+    }
 };
