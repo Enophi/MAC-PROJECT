1d964c925c84a16c4708ecfa7d129dd095840039
voknelim <voknelim@gmail.com>
2018-12-19 14:55:04 +0100

    modif port et supression user

--- APP/src/config/Config.ts
+++ APP/src/config/Config.ts
@@ -7,7 +7,7 @@ let env = process.env.NODE_ENV || 'development';
 export let config: IConfig = {
     name: 'recipe-made-simple',
     version: '1.0.0',
-    port: 3000,
+    port: 6666,
     env: 'dev',
     neo_url: 'bolt://localhost:7687',
     neo_user: 'neo4j',
