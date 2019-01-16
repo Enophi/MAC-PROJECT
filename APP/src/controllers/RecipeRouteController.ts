223224dc99a425fdbdc999951068fd9c353079bd
KDVL <kevin.dovale@heig-vd.ch>
2019-01-16 15:10:47 +0100

    Recipe : added bind params

--- APP/src/controllers/RecipeRouteController.ts
+++ APP/src/controllers/RecipeRouteController.ts
@@ -4,51 +4,29 @@ import { DatabaseController } from "./DatabaseController";
 export default class RecipeRouteController {
 
   public getAllRecipes(req: restify.Request, res: restify.Response, next: restify.Next) {
-
-      let query: string = "MATCH r=()-->() RETURN r LIMIT 25"
+      let query: string = "MATCH r=()-->() RETURN r" // LIMIT 25 ?
       DatabaseController.getInstance().makeCipherQuery(query, 'r', result => {
-
-          // TODO Do something with result, map with real Object, clean it, whatever...
-
           res.json(200, result);
       });
   }
 
   public getRecipe(req: restify.Request, res: restify.Response, next: restify.Next) {
-
-      let id:string = req.params.id;
-
-      let query: string = 'MATCH m = (r:Recipe)--() WHERE  ID(r) = '+ id +' RETURN m';
-
+      let query: string = 'MATCH m = (r:Recipe)--() WHERE  ID(r) = toInteger($id) RETURN m';
       DatabaseController.getInstance().makeCipherQuery(query, 'm', result => {
-          // TODO Do something with result, map with real Object, clean it, whatever...
           res.json(200, result);
-      });
+      }, {'id': req.params.id});
   }
 
-    /*public getAllRecipes2(req: restify.Request, res: restify.Response, next: restify.Next) {
-        DatabaseController.getInstance().getAll("Recipe", result => {
-
-            // TODO Do something with result, map with real Object, clean it, whatever...
-
-            res.json(200, result);
-        });
-    }*/
-
   public deleteRecipe(req: restify.Request, res: restify.Response, next: restify.Next) {
-    let id:string = req.params.id;
-
     //drop p and p's relations
-    let query: string = 'MATCH (p:Recipe) WHERE ID(p) = '+ id +' OPTIONAL MATCH (p)-[r]-() DELETE r,p';
-
+    let query: string = 'MATCH (p:Recipe) WHERE ID(p) = toInteger($id)'
+                       +'OPTIONAL MATCH (p)-[r]-() DELETE r,p';
     DatabaseController.getInstance().makeCipherQuery(query, 'm', result => {
-        // TODO Do something with result, map with real Object, clean it, whatever...
-        res.json(200, result);
-    });
+        res.json(200, 1);
+    }, {'id': req.params.id});
   }
 
   public updateRecipe(req: restify.Request, res: restify.Response, next: restify.Next) {
-
       let id:string = req.params.id;
 
       let query: string = ''; // TODO
@@ -57,7 +35,6 @@ export default class RecipeRouteController {
           // TODO Do something with result, map with real Object, clean it, whatever...
           res.json(200, result);
       });
-
   }
 
   public addRecipe(req: restify.Request, res: restify.Response, next: restify.Next) {
@@ -87,51 +64,54 @@ export default class RecipeRouteController {
       }
 
       //create recipe
-      let query:string = "CREATE (r:Recipe {name: '"+ recipe.name +"', preparation: "+ recipe.preparation +"}) RETURN r"
+      let query:string = "CREATE (r:Recipe {name: $name, preparation: $preparation}) RETURN r"
       DatabaseController.getInstance().makeCipherQuery(query, 'r', result => {
-        //user relation
-        let queryRel:string = "MATCH (r:Recipe),(u:User) "
-                              +" WHERE r.name = '"+ recipe.name +"'  AND ID(u) = " + user
-                              + " CREATE (u)-[rel:PUBLISH]->(r) "
-                              +"RETURN rel";
-
 
         ingredients.forEach(ing => {
           //check if ingredient exist
           let query:string = 'MATCH (i:Ingredient)'
-                             + 'WHERE  i.name = "'+ ing.name
-                             + '" RETURN i';
+                             + 'WHERE  i.name = $name'
+                             + ' RETURN i';
 
           DatabaseController.getInstance().makeCipherQuery(query, 'i', result => {
 
-            let queryRel:string = "MATCH (r:Recipe),(i:Ingredient) "
-                                  +" WHERE r.name = '"+ recipe.name +"'  AND i.name = '" + ing.name
-                                  + "' CREATE (r)-[rel:HAS {quantity: " + ing.quantity + ", unit:'"+ ing.unit +"'}]->(i) "
+            let queryRel:string = " MATCH (r:Recipe),(i:Ingredient) "
+                                  +"WHERE r.name = $recipe  AND i.name = $ing "
+                                  +"CREATE (r)-[rel:HAS {quantity: toInteger($quantity), unit: $unit}]->(i) "
                                   +"RETURN rel";
+            let paramsRel = {'recipe': recipe.name, 'ing': ing.name, 'quantity': ing.quantity, 'unit': ing.unit};
 
             //create Ingredient and relation
             if(result.length == 0){
-              let queryInsert:string = "CREATE (i:Ingredient {name:'"+ ing.name +"'}) RETURN i";
-              DatabaseController.getInstance().makeCipherQuery(queryInsert, 'i', result => {
-                DatabaseController.getInstance().makeCipherQuery(queryRel, 'rel', result => {});
-              });
+              DatabaseController.getInstance().makeCipherQuery("CREATE (i:Ingredient {name: $name }) RETURN i", 'i',
+              result => {
+                DatabaseController.getInstance().makeCipherQuery(queryRel, 'rel', result => {}, paramsRel);
+              }, {'name': ing.name} );
 
             }else{ //ingredients exist
-              //create rel
-              DatabaseController.getInstance().makeCipherQuery(queryRel, 'rel', result => {});
+              //create only rel
+              DatabaseController.getInstance().makeCipherQuery(queryRel, 'rel', result => {}, paramsRel);
             }
-          });
-        });
+          }, {'name': ing.name});
+
+        }); //end forEach
+
+        //user relation
+        let queryRel:string = "MATCH (r:Recipe),(u:User) "
+                              +" WHERE r.name = $name  AND ID(u) = toInteger($user)"
+                              + " CREATE (u)-[rel:PUBLISH]->(r) "
+                              +"RETURN rel";
 
         DatabaseController.getInstance().makeCipherQuery(queryRel, 'rel', result => {
           res.json(200, 1);
-        });
-      });
+        }, {'name': recipe.name, 'user': user});
+
+      }, { 'name': recipe.name, 'preparation':recipe.preparation });
   }
 
 
-/*  private addIngredientWithRelation(ing:any, recipe:string){
+  private addIngredientWithRelation(ing:any, recipe:string){
 
-}*/
+  }
 
 }
